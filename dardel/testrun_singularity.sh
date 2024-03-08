6c1b39525270cc6b3abff4f4c2100748d1ed0891
andersla <anders.p.larsson@uu.se>
2024-03-08 14:24:39 +0100

    updates

--- dardel/testrun_singularity.sh
+++ dardel/testrun_singularity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./run_cellprofiler_singularity_dardel.sh \
-      -d /cpp_work/input/9199 \
-      -o /cpp_work/output/9199 \
+      -d /cpp_work/input/9395 \
+      -o /cpp_work/output/9395 \
       -r /cpp_work/results/P101336/3173/6803 \
       -w 2 \
       -e 10
